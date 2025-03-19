8dd95999769dbc9c681c241878a175d5350fb89e
driptaroop das <driptaroop.das@gmail.com>
2025-03-19 14:48:48 +0100

    fix auth server

--- application/auth-server/src/main/kotlin/org/dripto/application/service/auth/App.kt
+++ application/auth-server/src/main/kotlin/org/dripto/application/service/auth/App.kt
@@ -1,29 +1,93 @@
 package org.dripto.application.service.auth
 
+import com.nimbusds.jose.jwk.JWKSet
+import com.nimbusds.jose.jwk.RSAKey
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet
+import com.nimbusds.jose.jwk.source.JWKSource
+import com.nimbusds.jose.proc.SecurityContext
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.core.annotation.Order
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.oauth2.core.AuthorizationGrantType
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer
 import org.springframework.security.web.SecurityFilterChain
+import java.security.KeyPair
+import java.security.KeyPairGenerator
+import java.security.interfaces.RSAPrivateKey
+import java.security.interfaces.RSAPublicKey
+import java.util.*
 
 @SpringBootApplication
 class App {
-    @Bean
-    @Order(1)
-    fun authServerSecurityFilterChain(http: HttpSecurity): SecurityFilterChain = http.with(OAuth2AuthorizationServerConfigurer.authorizationServer(), Customizer.withDefaults()).build()
 
     @Bean
-    @Order(2)
-    fun standardSecurityFilterChain(http: HttpSecurity): SecurityFilterChain =
-        http.authorizeHttpRequests {
-            it.requestMatchers("/actuator/**").permitAll()
-            it.anyRequest().authenticated()
-        }
+    fun standardSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        return http.with(OAuth2AuthorizationServerConfigurer.authorizationServer(), Customizer.withDefaults())
+            .authorizeHttpRequests {
+                it.requestMatchers("/actuator/**").permitAll()
+                it.anyRequest().authenticated()
+            }
             .formLogin { it.disable() }
             .csrf { it.disable() }.build()
+    }
+
+    @Bean
+    fun registeredClientRepository(): RegisteredClientRepository {
+        val k6 = createClient("k6")
+        val accountService = createClient("account-service")
+        val userService = createClient("user-service")
+        val notificationService = createClient("notification-service")
+        val transactionService = createClient("transaction-service")
+        return InMemoryRegisteredClientRepository(k6, accountService, userService, notificationService, transactionService)
+    }
+
+    @Bean
+    fun jwkSource(): JWKSource<SecurityContext> {
+        val keyPair: KeyPair = KeyPairGenerator.getInstance("RSA").apply { initialize(2048) }.genKeyPair()
+        val publicKey = keyPair.public as RSAPublicKey
+        val privateKey = keyPair.private as RSAPrivateKey
+        val rsaKey = RSAKey.Builder(publicKey)
+            .privateKey(privateKey)
+            .keyID(UUID.randomUUID().toString())
+            .build()
+        val jwkSet = JWKSet(rsaKey)
+        return ImmutableJWKSet(jwkSet)
+    }
+
+    @Bean
+    fun jwtDecoder(jwkSource: JWKSource<SecurityContext>): JwtDecoder {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource)
+    }
+
+    @Bean
+    fun tokenCustomizer(): OAuth2TokenCustomizer<JwtEncodingContext> {
+        return OAuth2TokenCustomizer { context ->
+            if (OAuth2TokenType.ACCESS_TOKEN == context.tokenType) {
+                // Add custom claim to the access token
+                context.claims.claim("hello1", "world1")
+            }
+        }
+    }
+
+    private fun createClient(client: String): RegisteredClient = RegisteredClient.withId(client)
+        .clientId(client)
+        .clientSecret("{noop}$client-secret")
+        .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+        .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+        .scope("local")
+        .build()
 }
 
 fun main(args: Array<String>) {
